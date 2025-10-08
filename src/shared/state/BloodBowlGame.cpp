0ab0de5a4fa07e76bf731387e70398acbe273f83
HDieTox <tom.compagnon2003@gmail.com>
2025-10-08 17:30:41 +0200

    UPDATE dia (methods) + BEGIN CPP

--- src/shared/state/BloodBowlGame.cpp
+++ src/shared/state/BloodBowlGame.cpp
@@ -1,6 +1,23 @@
 #include "BloodBowlGame.h"
 
 namespace  state {
+    BloodBowlGame::BloodBowlGame (const Board &board, Team* teams, const int turnCounter) :
+    board(board), teams(teams), turnCounter(turnCounter) {
+        this->currentState = nullptr;
+    }
+
+    void BloodBowlGame::setCurrentState(AbstractState *state) {
+        if (currentState != nullptr) {
+            delete currentState;
+        }
+        currentState = state;
+    }
+
+    AbstractState *BloodBowlGame::getCurrentState() {
+        return currentState;
+    }
+
+
 
 
 }
--- src/shared/state/Setup.cpp
+++ src/shared/state/Setup.cpp
@@ -1,5 +1,40 @@
 #include "Setup.h"
 
 namespace state {
+    Setup::Setup(BloodBowlGame *game) : AbstractState(game) {
+    }
+
+    int Setup::nbPieceOnBoard(Team team) {
+        int count = 0;
+        for (Piece piece : team.getPieces()) {
+            if (piece.getSquare() != nullptr) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    int Setup::nbPieceOnLine(Team team) {
+        std::vector<Square> linesSquares;
+        // Example coordinates for the 7 squares
+        std::vector<std::pair<int, int>> fixedCoords = {
+            {13, 4}, {13, 5}, {13, 6}, {13, 7}, {13, 8}, {13, 9}, {13, 10}
+        };
+
+        for (const auto& coord : fixedCoords) {
+            linesSquares.push_back(Square(coord.first, coord.second));
+        }
+        int count = 0;
+        for (Piece piece : team.getPieces()) {
+            if (piece.getSquare() != nullptr && piece.getSquare()->isIn(linesSquares)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    int Setup::nbPieceOnBoard(Team team) {
+
+    }
 
 }
