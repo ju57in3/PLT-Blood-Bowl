8061d6b056dba183b86d369fa94003104e4e6e14
HDieTox <tom.compagnon2003@gmail.com>
2025-10-09 10:59:23 +0200

    ADD Setup State

--- src/shared/state/Square.cpp
+++ src/shared/state/Square.cpp
@@ -1,6 +1,19 @@
 #include "Square.h"
 
+#include <vector>
+
 namespace state {
 
+    bool Square::isIn(std::vector<Square> list) {
+        if (list.size() == 0) {
+            return false;
+        }
+        for (Square square : list) {
+            if (this->x == square.getX() && this->y == square.getY()) {
+                return true;
+            }
+        }
+        return false;
+    }
 
 }
