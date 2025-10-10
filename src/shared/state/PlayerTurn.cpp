ad715107f35d0eaab34d937b325a02423b15de13
HDieTox <tom.compagnon2003@gmail.com>
2025-10-10 09:30:45 +0200

    ADD PlayerTurn.cpp

--- src/shared/state/PlayerTurn.cpp
+++ src/shared/state/PlayerTurn.cpp
@@ -1,24 +1,86 @@
 #include "PlayerTurn.h"
 
 #include "BloodBowlGame.h"
+#include "EndGame.h"
+#include "HalfTime.h"
+#include "Setup.h"
 #include "Team.h"
 
 namespace state {
-
     PlayerTurn::PlayerTurn(BloodBowlGame* game) : AbstractState(game) {
         isBlitzAvailable = true;
         isTurnOver = false;
         isTouchDown = false;
+        endTurn = false;
         teamId = game->getCurrentTeam().getTeamId();
 
         availableCharacters = game->getCurrentTeam().getPlayableCharacter();
     }
 
-    void update() {
+    void PlayerTurn::update() {
+        if (game->getTurnCounter() == 16) {
+            if (isTurnOver) {
+                restoreCharactersStatus();
+                switchTeam();
+                game->setTurnCounter(game->getTurnCounter() + 1);
+                game->setCurrentState(new state::HalfTime(game));
+            }
+            if (isTouchDown) {
+                game->getCurrentTeam().setScore(game->getCurrentTeam().getScore() + 1);
+                restoreCharactersStatus();
+                switchTeam();
+                game->setTurnCounter(game->getTurnCounter() + 1);
+                game->setCurrentState(new state::HalfTime(game));
+            }
+            if (endTurn) {
+                restoreCharactersStatus();
+                switchTeam();
+                game->setTurnCounter(game->getTurnCounter() + 1);
+                game->setCurrentState(new state::HalfTime(game));
+            }
+        } else if (game->getTurnCounter() == 32) {
+            if (isTurnOver) {
+                restoreCharactersStatus();
+                switchTeam();
+                game->setCurrentState(new state::EndGame(game));
+            }
+            if (isTouchDown) {
+                game->getCurrentTeam().setScore(game->getCurrentTeam().getScore() + 1);
+                restoreCharactersStatus();
+                switchTeam();
+                game->setCurrentState(new state::EndGame(game));
+            }
+            if (endTurn) {
+                restoreCharactersStatus();
+                switchTeam();
+                game->setTurnCounter(game->getTurnCounter() + 1);
+                game->setCurrentState(new state::EndGame(game));
+            }
+        } else {
+            if (isTurnOver) {
+                restoreCharactersStatus();
+                switchTeam();
+                game->setTurnCounter(game->getTurnCounter() + 1);
+                game->setCurrentState(new state::HalfTime(game));
+            }
+            if (isTouchDown) {
+                game->getCurrentTeam().setScore(game->getCurrentTeam().getScore() + 1);
+                restoreCharactersStatus();
+                switchTeam();
+                game->setTurnCounter(game->getTurnCounter() + 1);
+                game->setCurrentState(new state::HalfTime(game));
+            }
+            if (endTurn) {
+                restoreCharactersStatus();
+                switchTeam();
+                game->setTurnCounter(game->getTurnCounter() + 1);
+                game->setCurrentState(new state::HalfTime(game));
+            }
+        }
 
     }
 
-    void PlayerTurn::restoreCharacterStatus() {
+    void PlayerTurn::restoreCharactersStatus() {
         std::vector<Character> characters = game->getCurrentTeam().getCharacters();
         for (int i = 0; i < characters.size(); i++)
         {
@@ -30,6 +92,15 @@ namespace state {
             {
                 characters[i].setStatus(knockedDown);
             }
+        }
+    }
+
+
+    void PlayerTurn::switchTeam() {
+        if (game->getCurrentTeam().getTeamId() == game->getTeamA().getTeamId()) {
+            game->setCurrentTeam(game->getTeamB());
+        } else {
+            game->setCurrentTeam(game->getTeamA());
         }
     }
 }
