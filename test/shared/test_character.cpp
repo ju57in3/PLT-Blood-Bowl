9e12ce3bf2936c27bb139f85505a2e8b6ed5a2b9
MatteoEnsea <matt.doffe@gmail.com>
2025-10-16 11:41:11 +0200

    Update test_character.cpp

--- test/shared/test_character.cpp
+++ test/shared/test_character.cpp
@@ -8,7 +8,15 @@ using namespace state;
 BOOST_AUTO_TEST_CASE(TestCharacter)
 {
     Character human("Player 1", "human", 6, 3, 3, 8);
-    human.getStatus();
+    BOOST_CHECK_EQUAL(human.getStatus(),bench);
+    human.setStatus(playable);
+    BOOST_CHECK_EQUAL(human.getStatus(),playable);
+
+    BOOST_CHECK(human.getPosition() == std::make_pair(-1,-1));
+    human.setPosition(std::make_pair(1,2));
+    BOOST_CHECK(human.getPosition() == std::make_pair(1,2));
+
+    BOOST_CHECK(not human.hasBall());
 }
 
 /* vim: set sw=2 sts=2 et : */
