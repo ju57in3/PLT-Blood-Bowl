ce8571c3f5197e51550063fcd6ffa18427f58f4a
ju57in3 <justine.hazan@ensea.fr>
2025-10-19 19:39:58 +0200

    majs tests setup et kickoff + maj Rapport1.final.md

--- test/shared/test_setup.cpp
+++ test/shared/test_setup.cpp
@@ -10,22 +10,24 @@ using namespace state;
 BOOST_AUTO_TEST_CASE(TestSetup)
 {
     Team teamA(1, "Humans", 3);
+    Team teamB(2, "Orcs", 2);
+
     for (int i = 0; i < 11; i++) {
         Character c("H" + std::to_string(i+1), "Human", 6, 3, 3, 8);
         c.setStatus(playable);
-        if (i < 3) c.setPosition({13, 4+i});      // Line
-        else if (i < 5) c.setPosition({12, 2+i}); // Top
-        else c.setPosition({8, 4+i});             // Middle
+        if (i < 3) c.setPosition({12, 4+i});      // line
+        else if (i < 5) c.setPosition({13, 4+i}); // top
+        else c.setPosition({8, 4+i});             // middle
         teamA.addCharacter(c);
     }
 
-    BloodBowlGame game(teamA, teamA);
+    BloodBowlGame game(teamA, teamB);
     Setup setup(&game);
 
     BOOST_CHECK(setup.isValidSetup(teamA));
     BOOST_CHECK_EQUAL(setup.nbCharacterOnBoard(teamA), 11);
-    BOOST_CHECK_EQUAL(setup.nbCharacterOnLine(teamA), 2);
-    BOOST_CHECK_EQUAL(setup.nbCharacterOnTop(teamA), 3);
+    BOOST_CHECK_EQUAL(setup.nbCharacterOnLine(teamA), 3);
+    BOOST_CHECK_EQUAL(setup.nbCharacterOnTop(teamA), 2);
     BOOST_CHECK_EQUAL(setup.nbCharacterOnBottom(teamA), 0);
 
     setup.endSetup();
