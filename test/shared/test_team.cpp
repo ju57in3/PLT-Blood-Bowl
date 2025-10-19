c77201fa7911b7a40d564b3e7ee46bff47729d95
MatteoEnsea <matt.doffe@gmail.com>
2025-10-19 21:26:46 +0200

    fix test_team.cpp

--- test/shared/test_team.cpp
+++ test/shared/test_team.cpp
@@ -34,8 +34,7 @@ BOOST_AUTO_TEST_CASE(TestTeam) {
     BOOST_CHECK_EQUAL(teamA.getCharacters()[0]->getArmor(),8);
 
     BOOST_CHECK_EQUAL(size(teamA.getPlayableCharacter()),0);
-    teamA.getCharacters()[0]->setStatus(playable);
-    //human.setStatus(playable); // We have to use the line over to have this result
+    teamA.getCharacters().back()->setStatus(playable);
     BOOST_CHECK_EQUAL(size(teamA.getPlayableCharacter()),1);
 
     std::ostringstream oss;
