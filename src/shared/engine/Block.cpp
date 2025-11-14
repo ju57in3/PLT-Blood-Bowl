46f244d455a629bd89191716f61a95c42a211d32
guilhem <guilhem.boularan@ensea.fr>
2025-11-14 21:47:54 +0100

    Merge de engine.dia Ajout de la gestion du blocage et des blessures

--- src/shared/engine/Block.cpp
+++ src/shared/engine/Block.cpp
@@ -1,31 +1,33 @@
 #include "Block.h"
 
-namespace engine {
-    Block::Block(state::Character attacker, state::Character defender):attacker(attacker), defender(defender){}
+#include <state/PlayerTurn.h>
 
-    BlockResult Block::rollBlockDice()
+namespace engine {
+    Block::Block(std::shared_ptr<state::Character> attacker, std::shared_ptr<state::Character> defender):attacker(attacker), defender(defender){}
+
+    void Block::rollBlockDice()
     {
         int diceResult;
         int range = 6; //Range lancé de Dé pour le Block
         int roll1 = rollDice(6);
         diceResult = roll1; //Si l'attaquant et le défenseur ont le même nombre de points de force, il n'y a qu'un seul lancé de dé
 
-        if (attacker.getStrength()>= defender.getStrength())
+        if (attacker->getStrength()>= defender->getStrength())
         {
             int roll2 = rollDice(6);
             diceResult = std::max(roll1,roll2);  // Le choix arrangeant le plus l'attaquant sont les résultats élevés
-            if (attacker.getStrength()>= 2*defender.getStrength())
+            if (attacker->getStrength()>= 2*defender->getStrength())
             {
                 int roll3 = rollDice(6);
                 diceResult = std::max(roll3,diceResult);
             }
         }
 
-        else if (attacker.getStrength() <= defender.getStrength())
+        else if (attacker->getStrength() <= defender->getStrength())
         {
             int roll2 = rollDice(6);
             diceResult = std::min(roll1,roll2); // Le choix arrangeant le plus le défenseur sont les résultats faibles
-            if (attacker.getStrength()>= 2*defender.getStrength())
+            if (attacker->getStrength()>= 2*defender->getStrength())
             {
                 int roll3 = rollDice(6);
                 diceResult = std::min(roll3,diceResult);
@@ -34,22 +36,79 @@ namespace engine {
 
         if (diceResult == 1)
         {
-            return AttackerDown;
+            blockResult = AttackerDown;
         }
-        if (diceResult == 2){
-            return BothDown;
+        if (diceResult == 2)
+        {
+            blockResult = BothDown;
         }
         if (diceResult == 3 || diceResult == 4)
         {
-            return Pushed;
+            blockResult = Pushed;
         }
         if (diceResult == 5)
         {
-            return DefenderStumbles;
+            blockResult = DefenderStumbles;
         }
         if (diceResult == 6)
         {
-            return DefenderDown;
+            blockResult = DefenderDown;
+        }
+    }
+
+    void Block::resolveInjury(std::shared_ptr<state::Character> targetCharacter)
+    {
+        int roll1 = rollDice(6);
+        int roll2 = rollDice(6);
+        if (roll1 + roll2 >= targetCharacter->getArmor())
+        {
+            roll1 = rollDice(6);
+            roll2 = rollDice(6);
+            if (roll1 + roll2 <= 7)
+            {
+                targetCharacter->setStatus(state::CharacterStatus::knockedDown);
+            }
+            else if (roll1 + roll2 <= 9)
+            {
+                targetCharacter->setStatus(state::CharacterStatus::injured);
+            }
+            else if (roll1 + roll2 <= 12)
+            {
+                targetCharacter->setStatus(state::CharacterStatus::ko);
+            }
+        }
+    }
+
+    void Block::execute(std::shared_ptr<state::BloodBowlGame> game)
+    {
+        rollBlockDice();
+        if (blockResult == AttackerDown || blockResult == BothDown)
+        {
+            resolveInjury(attacker);
+            //TurnOver !!  Utilisation de la fonction setTurnOver de PlayerTurn (Voir compléter fonction update() de PlayerTurn! -> if (turnOver))
+        }
+        if (blockResult == Pushed || blockResult == DefenderStumbles)
+        {
+            std::pair<int,int> newTarget;
+            std::pair<int,int> actualTarget;
+
+            actualTarget.first = defender->getPosition().first;
+            actualTarget.second = defender->getPosition().second;
+
+            newTarget.first = defender->getPosition().first + (defender->getPosition().first - attacker->getPosition().first);
+            newTarget.second = defender->getPosition().second;
+
+            defender->setPosition(newTarget);
+            attacker->setPosition(actualTarget); //Pour l'instant, on considère que l'attaquant prend obligatoirement la place du défenseur. Il faudra coder une requête auprès de l'utilisateur.
+        }
+        if (blockResult == DefenderDown || blockResult == BothDown)
+        {
+            resolveInjury(defender);
+        }
+        if (blockResult == DefenderStumbles)
+        {
+            //Gestion Esquive du plaquage. A faire plus tard. En attendant, on considère qu'il est plaqué à chaque fois
+            resolveInjury(defender);
         }
     }
 
