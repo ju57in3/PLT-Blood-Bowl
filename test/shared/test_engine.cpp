11b1454284effd70d5fc6a0daf6b2dc998f599e8
MattDff <matt.doffe@gmail.com>
2026-01-15 11:58:33 +0100

    fin seance 15/01

--- test/shared/test_engine.cpp
+++ test/shared/test_engine.cpp
@@ -8,5 +8,11 @@ using namespace engine;
 using namespace state;
 
 BOOST_AUTO_TEST_CASE(TestEngine) {
+    Team teamA(1, "Humans", 3);
+    Team teamB(2, "Orcs", 2);
 
+    auto gamePtr = std::make_shared<BloodBowlGame>(teamA, teamB);
+    Engine engine(gamePtr);
+
+    BOOST_CHECK(engine.getGameState() != nullptr);
 }
